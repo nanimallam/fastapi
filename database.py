6d8f3b52ea41cd80a67e913fd418ddc9ed3e3aa9
nanimallam <rahulmallam1432@gmail.com>
2026-08-07 16:06:16 +0530

    edited

--- database.py
+++ database.py
@@ -1,16 +1,19 @@
-import os
-from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+import os
 
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine( DATABASE_URL,
+engine = create_engine(
+    DATABASE_URL,
     connect_args={
         "ssl": {}
-    })
+    },
+    pool_pre_ping=True
+)
 
 SessionLocal = sessionmaker(
     autocommit=False,
@@ -19,3 +22,11 @@ SessionLocal = sessionmaker(
 )
 
 Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
