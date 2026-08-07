6896531926305eb08b4050d36b80a2273c7a5d26
nanimallam <rahulmallam1432@gmail.com>
2026-08-07 15:48:49 +0530

    edited

--- database.py
+++ database.py
@@ -5,16 +5,12 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 load_dotenv()
 
-from sqlalchemy import create_engine
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-DATABASE_URL = "mysql+pymysql://avnadmin:YOUR_PASSWORD@mysql-fastapi-rahulmallam1432-a239.k.aivencloud.com:13434/defaultdb"
-
-engine = create_engine(
-    DATABASE_URL,
+engine = create_engine( DATABASE_URL,
     connect_args={
         "ssl": {}
-    }
-)
+    })
 
 SessionLocal = sessionmaker(
     autocommit=False,
